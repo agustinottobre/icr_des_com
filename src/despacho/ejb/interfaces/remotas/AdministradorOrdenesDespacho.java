c8510e97ec39c112ee99a42437ced169d8a350b1
PabloRiesco <pablo.j.riesco@gmail.com>
2014-11-01 15:13:43 -0300

    Persistencia OrdenesDespacho

--- src/despacho/ejb/interfaces/remotas/AdministradorOrdenesDespacho.java
+++ src/despacho/ejb/interfaces/remotas/AdministradorOrdenesDespacho.java
@@ -1,10 +1,13 @@
 package despacho.ejb.interfaces.remotas;
 
+import java.util.List;
+
 import javax.ejb.Remote;
 
 @Remote
 public interface AdministradorOrdenesDespacho {
 	
-	public long obtenerCantidadOrdenesDespacho();
+	public String cargarOrdenDespacho (String codigo_despacho, String codigo_venta, String id_portal, 
+			String id_monitoreo, List<String[][]> items);
 
 }
