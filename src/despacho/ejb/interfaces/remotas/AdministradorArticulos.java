0db6cc57cd2f500cbbc1ebfa2a45622609f0b408
echarly02 <echarly02@gmail.com>
2014-11-01 17:43:14 -0300

    Nuevos metodos en interfaces remotas

--- src/despacho/ejb/interfaces/remotas/AdministradorArticulos.java
+++ src/despacho/ejb/interfaces/remotas/AdministradorArticulos.java
@@ -2,9 +2,15 @@ package despacho.ejb.interfaces.remotas;
 
 import javax.ejb.Remote;
 
+import dto.ArticuloDTO;
+
 //interfaz del session bean AdministradorArticulos
 @Remote
 public interface AdministradorArticulos {
 	
 	public String testEJB();
+	
+	public ArticuloDTO altaArticulo(String codigo, String nombre, String idModulo);
+	
+	public ArticuloDTO BuscarArticulo(int idArticulo);
 }
--- src/despacho/ejb/interfaces/remotas/AdministradorOrdenesDespacho.java
+++ src/despacho/ejb/interfaces/remotas/AdministradorOrdenesDespacho.java
@@ -1,13 +1,13 @@
 package despacho.ejb.interfaces.remotas;
 
 import java.util.List;
-
 import javax.ejb.Remote;
+import dto.OrdenDespachoDTO;
 
 @Remote
 public interface AdministradorOrdenesDespacho {
 	
-	public String altaOrdenDespacho (String codigo_despacho, String codigo_venta, String id_portal, 
+	public OrdenDespachoDTO altaOrdenDespacho (String codigo_despacho, String codigo_venta, String id_portal, 
 			String id_monitoreo, List<String[][]> items);
 	
 	//public buscarOrdenDespacho (String idOrdenDespacho);
