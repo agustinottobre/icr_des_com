a06671f051dde636cb50d8fdede2a45125cf1a95
PabloRiesco <pablo.j.riesco@gmail.com>
2014-11-01 18:17:50 -0300

    Movida de DTOs a Common

--- src/despacho/ejb/interfaces/remotas/AdministradorSolicitudesArticulo.java
+++ src/despacho/ejb/interfaces/remotas/AdministradorSolicitudesArticulo.java
@@ -2,8 +2,13 @@ package despacho.ejb.interfaces.remotas;
 
 import javax.ejb.Remote;
 
+import dto.ArticuloDTO;
+import dto.SolicitudArticuloDTO;
+
 @Remote
 public interface AdministradorSolicitudesArticulo {
 
 	public long obtenerCantidadSolicitudesArticulo();
+	
+	public SolicitudArticuloDTO nuevaSolicitud(ArticuloDTO articulo, int cantidad); 
 }
---
+++ src/dto/OrdenVentaDTO.java
@@ -0,0 +1,28 @@
+package dto;
+
+
+public class OrdenVentaDTO {
+	
+
+	private int idOrdenVenta;
+	
+
+	private PortalDTO portal;
+
+	public int getIdOrdenVenta() {
+		return idOrdenVenta;
+	}
+
+	public void setIdOrdenVenta(int idOrdenVenta) {
+		this.idOrdenVenta = idOrdenVenta;
+	}
+
+	public PortalDTO getPortal() {
+		return portal;
+	}
+
+	public void setPortal(PortalDTO portal) {
+		this.portal = portal;
+	}
+
+}
