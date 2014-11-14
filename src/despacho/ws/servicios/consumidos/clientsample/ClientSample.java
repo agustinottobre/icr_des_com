d01ad5a2b96d94cab4bc9dd26fd0928ffd37ca1b
Agustin Ottobre <agustin.ottobre@gmail.com>
2014-11-13 21:47:55 -0300

    arreglo para cliente ws

--- src/despacho/ws/servicios/consumidos/clientsample/ClientSample.java
+++ src/despacho/ws/servicios/consumidos/clientsample/ClientSample.java
@@ -1,13 +1,20 @@
 package despacho.ws.servicios.consumidos.clientsample;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
 import despacho.ws.servicios.consumidos.*;
 
 public class ClientSample {
+	
+//	private static String WS_URL = "http://25.13.74.32:8080/testdespachoWEB/ServidorEstadoEntregaBean?wsdl";
+	private static String WS_URL = "http://localhost:8080/testdespachoWEB/ServidorEstadoEntregaBean?wsdl";
 
 	public static void main(String[] args) {
+		try{
 	        System.out.println("***********************");
 	        System.out.println("Create Web Service Client...");
-	        ServidorEstadoEntregaBeanService service1 = new ServidorEstadoEntregaBeanService();
+	        ServidorEstadoEntregaBeanService service1 = new ServidorEstadoEntregaBeanService(new URL(WS_URL));
 	        System.out.println("Create Web Service...");
 	        ServidorEstadoEntregaBean port1 = service1.getServidorEstadoEntregaBeanPort();
 	        System.out.println("Call Web Service Operation...");
@@ -18,5 +25,8 @@ public class ClientSample {
 	        System.out.println("Server said: " + port2.notificarEntregaDespacho(92));
 	        System.out.println("***********************");
 	        System.out.println("Call Over!");
+		}catch(MalformedURLException mue){
+			mue.printStackTrace();
+		}
 	}
 }
